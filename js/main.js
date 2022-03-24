e49de95c70931054eab6eead1a8458bf5303ccd4
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-24 20:43:01 +0100

    modifiche-estetiche-denominazione-js

--- js/main.js
+++ js/main.js
@@ -11,10 +11,10 @@
             const self = this;
                 for(let i = 0; i < 10; i++){
                     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")           // quando risponderai allora mi andrai a eseguire la funzione qui sotto con argomento la risposta che hai appena ricevuto
-                    .then(function (response){                                                      // response è un oggetto che ci da i dati la configurazione l'header e la richiesta 
-                        const result = response.data.response; 
-                        self.risultato.push(result);
-                                                             //data è un oggetto formato da response e success
+                    .then(function (reply){                                                      // response è un oggetto che ci da i dati la configurazione l'header e la richiesta 
+                        const result = reply.data.response; 
+                        self.risultato.push(result);                    //mi pusho dentro l'array risultato la variabile result contenente 
+                                                                                                        //data è un oggetto formato da response e success
                     });
                 }
                 console.log(self.risultato)      
