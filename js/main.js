d983c243617552b5d70d71807124bf04014c860a
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-24 17:44:15 +0100

    esercizio-complex

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-const app = new Vue(
+ const app = new Vue(
     {
-        el: "app",
-        data:{},
-        methods:{},
-    }
-);
-for(let i = 0; i < 10; i++){
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")           // quando risponderai allora mi andrai a eseguire la funzione qui sotto con argomento la risposta che hai appena ricevuto
-    .then(function (response){                      
-        const result = response;
-        console.log(result.data.response)                                       //data è un oggetto formato da response e success
-    })
-}
+        el: "#app",
+        data:{
+            risultato: [],
+        },
+        methods:{
+        },
+
+        mounted(){
+            const self = this;
+                for(let i = 0; i < 10; i++){
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")           // quando risponderai allora mi andrai a eseguire la funzione qui sotto con argomento la risposta che hai appena ricevuto
+                    .then(function (response){                                                      // response è un oggetto che ci da i dati la configurazione l'header e la richiesta 
+                        const result = response.data.response; 
+                        self.risultato.push(result);
+                                                             //data è un oggetto formato da response e success
+                    });
+                }
+                console.log(self.risultato)      
+            
+    
+        }
+    
+    });
+ 
+
